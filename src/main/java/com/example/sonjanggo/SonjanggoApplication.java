20a2611d38a0541b136f6a5fc5a70aa7201d5c1c
yo0919 <0919yo@naver.com>
2024-07-13 18:35:21 +0900

    [chore] 깃허브 깃랩 테스트

--- src/main/java/com/example/sonjanggo/SonjanggoApplication.java
+++ src/main/java/com/example/sonjanggo/SonjanggoApplication.java
@@ -3,6 +3,7 @@ package com.example.sonjanggo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//
 @SpringBootApplication
 public class SonjanggoApplication {
 
